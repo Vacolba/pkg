a2e4e0694ccb458bf8ff21a56f48f5e9e55118af
Tomas J. Sahagun <tomas@grupomabis.com>
2018-11-05 08:26:38 +0100

    Add yaml label con config structs

--- model/config.go
+++ model/config.go
@@ -9,70 +9,70 @@ const (
 
 // Config is the config format for the main application.
 type Config struct {
-	Storage Storage `json:"storage"`
-	Web     Web     `json:"web"`
-	GRPC    GRPC    `json:"grpc"`
-	Logger  Logger  `json:"logger"`
-	OpenID  OpenID  `json:"auth"`
+	Storage Storage `json:"storage" yaml:"storage"`
+	Web     Web     `json:"web" yaml:"web"`
+	GRPC    GRPC    `json:"grpc" yaml:"grpc"`
+	Logger  Logger  `json:"logger" yaml:"logger"`
+	OpenID  OpenID  `json:"auth" yaml:"auth"`
 }
 
 // Storage holds app's storage configuration.
 type Storage struct {
-	DB      DBStorage    `json:"db"`
-	Session RedisStorage `json:"session"`
-	Cache   RedisStorage `json:"cache"`
+	DB      DBStorage    `json:"db" yaml:"db"`
+	Session RedisStorage `json:"session" yaml:"session"`
+	Cache   RedisStorage `json:"cache" yaml:"cache"`
 }
 
 // DBStorage settings for SQL Databse connection
 type DBStorage struct {
-	Host     string `json:"host"`
-	Port     int    `json:"port"`
-	User     string `json:"user"`
-	Password string `json:"password"`
-	Database string `json:"database"`
-	Key      string `json:"key"`
+	Host     string `json:"host" yaml:"host"`
+	Port     int    `json:"port" yaml:"port"`
+	User     string `json:"user yaml:"user"`
+	Password string `json:"password" yaml:"password"`
+	Database string `json:"database" yaml:"database"`
+	Key      string `json:"key" yaml:"key"`
 }
 
 // RedisStorage settings to connect to Redis
 type RedisStorage struct {
-	Type     string `json:"type"`
-	Master   string `json:"master"`
-	Host     string `json:"host"`
-	Port     int    `json:"port"`
-	Size     int    `json:"size"`
-	Database string `json:"database"`
-	HashKey  string `json:"key"`
-	BlockKey string `json:"blockkey"`
+	Type     string `json:"type" yaml:"type"`
+	Master   string `json:"master" yaml:"master"`
+	Host     string `json:"host" yaml:"host"`
+	Port     int    `json:"port" yaml:"port"`
+	Size     int    `json:"size" yaml:"size"`
+	Database string `json:"database" yaml:"database"`
+	HashKey  string `json:"key" yaml:"key"`
+	BlockKey string `json:"blockkey" yaml:"blockkey"`
 }
 
 // Web is the config format for the HTTP server.
 type Web struct {
-	HTTP           string   `json:"http"`
-	HTTPS          string   `json:"https"`
-	TLSCert        string   `json:"tlsCert"`
-	WS             string   `json:"ws"`
-	TLSKey         string   `json:"tlsKey"`
-	AllowedOrigins []string `json:"allowedOrigins"`
+	HTTP           string   `json:"http" yaml:"http"`
+	HTTPS          string   `json:"https" yaml:"https"`
+	TLSCert        string   `json:"tlsCert" yaml:"tlsCert"`
+	WS             string   `json:"ws" yaml:"ws"`
+	TLSKey         string   `json:"tlsKey" yaml:"tlsKey"`
+	AllowedOrigins []string `json:"allowedOrigins" yaml:"allowedOrigins"`
 }
 
 // GRPC is the config for the gRPC API.
 type GRPC struct {
-	Addr        string `json:"addr"`
-	TLSCert     string `json:"tlsCert"`
-	TLSKey      string `json:"tlsKey"`
-	TLSClientCA string `json:"tlsClientCA"`
+	Addr        string `json:"addr" yaml:"addr"`
+	TLSCert     string `json:"tlsCert" yaml:"tlsCert"`
+	TLSKey      string `json:"tlsKey" yaml:"tlsKey"`
+	TLSClientCA string `json:"tlsClientCA" yaml:"tlsClientCA"`
 }
 
 // OpenID Connector settings
 type OpenID struct {
-	ID       string `json:"id"`
-	Secret   string `json:"secret"`
-	Redirect string `json:"redirect"`
-	Discover string `json:"discover"`
+	ID       string `json:"id" yaml:"id"`
+	Secret   string `json:"secret" yaml:"secret"`
+	Redirect string `json:"redirect" yaml:"redirect"`
+	Discover string `json:"discover" yaml:"discover"`
 }
 
 // Logger holds configuration required to customize logging for vreports.
 type Logger struct {
-	Level  string `json:"level"`  // Level sets logging level severity.
-	Format string `json:"format"` // Format specifies the format to be used for logging.
+	Level  string `json:"level" yaml:"level"`   // Level sets logging level severity.
+	Format string `json:"format" yaml:"format"` // Format specifies the format to be used for logging.
 }
